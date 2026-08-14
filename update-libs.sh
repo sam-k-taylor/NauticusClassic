45983c27ce0613949dd4b66eb5d75d9b6655d206
Sam Taylor <sam.taylor@technologywithin.com>
2026-08-14 10:56:23 +0100

    libs folder clean up

--- update-libs.sh
+++ update-libs.sh
@@ -14,29 +14,20 @@ SOURCES=(
 	"Libs/LibStub|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/LibStub"
 	"Libs/CallbackHandler-1.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/CallbackHandler-1.0"
 	"Libs/AceAddon-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceAddon-3.0"
-	"Libs/AceBucket-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceBucket-3.0"
 	"Libs/AceComm-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceComm-3.0"
 	"Libs/AceConfig-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceConfig-3.0"
 	"Libs/AceConsole-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceConsole-3.0"
 	"Libs/AceDB-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceDB-3.0"
-	"Libs/AceDBOptions-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceDBOptions-3.0"
 	"Libs/AceEvent-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceEvent-3.0"
 	"Libs/AceGUI-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceGUI-3.0"
-	"Libs/AceHook-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceHook-3.0"
 	"Libs/AceLocale-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceLocale-3.0"
-	"Libs/AceSerializer-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceSerializer-3.0"
-	"Libs/AceTab-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceTab-3.0"
 	"Libs/AceTimer-3.0|https://raw.githubusercontent.com/WoWUIDev/Ace3/master/AceTimer-3.0"
 	"Libs/LibDataBroker-1.1|https://raw.githubusercontent.com/tekkub/libdatabroker-1-1/master"
 	"Libs/LibDBIcon-1.0|https://raw.githubusercontent.com/wowace-clone/LibDBIcon-1.0/main/LibDBIcon-1.0"
-	# Only the files HereBeDragons' own repo actually contains; the vendored
-	# LibStub/CallbackHandler-1.0 subfolders under Libs/HereBeDragons are
-	# leftovers from an old external-libs fetch, not part of that repo, and
-	# CHANGELOG.md isn't published there either, so none of those are synced.
+	# Only the files HereBeDragons' own repo actually contains and that this
+	# addon's .toc actually loads (HereBeDragons-2.0.lua, HereBeDragons-Pins-2.0.lua).
 	"Libs/HereBeDragons/HereBeDragons-2.0.lua|https://raw.githubusercontent.com/Nevcairiel/HereBeDragons/master/HereBeDragons-2.0.lua"
 	"Libs/HereBeDragons/HereBeDragons-Pins-2.0.lua|https://raw.githubusercontent.com/Nevcairiel/HereBeDragons/master/HereBeDragons-Pins-2.0.lua"
-	"Libs/HereBeDragons/HereBeDragons-Migrate.lua|https://raw.githubusercontent.com/Nevcairiel/HereBeDragons/master/HereBeDragons-Migrate.lua"
-	"Libs/HereBeDragons/HereBeDragons.toc|https://raw.githubusercontent.com/Nevcairiel/HereBeDragons/master/HereBeDragons.toc"
 )
 
 changed=0
@@ -71,8 +62,10 @@ for entry in "${SOURCES[@]}"; do
 			rel="${file#"$path"/}"
 			sync_one "$file" "$url_part/$rel"
 		done < <(find "$path" -type f -print0)
-	else
+	elif [ -f "$path" ]; then
 		sync_one "$path" "$url_part"
+	else
+		echo "SKIPPED $path  (no longer present locally)" >&2
 	fi
 done
 
